cf1a54d19136d0a4ef48308a0ac34acb4ab541bd
FrancisCarella <francescodecarocarella@gmail.com>
2022-11-09 11:25:59 +0100

    esercitazione 08-11-22

--- 08-11-22/script.js
+++ 08-11-22/script.js
@@ -1,25 +1,22 @@
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=150")
-  .then((response) => response.json())
-  .then((pokemon) => {
-    pokemon.results.forEach((element) =>
-      fetch(element.url).then((response) =>
-        response.json().then((pokemon) => createPokemonCard(pokemon))
-      )
-    );
-  });
-
 const bodyEl = document.querySelector("main");
 
 const createPokemonCard = (data) => {
   const cardEl = document.createElement("div");
   cardEl.className = "card";
+  cardEl.classList.add(`bg-${data.types[0].type.name}`);//Classe per assegnare il background-color
 
   const imgEl = document.createElement("img");
   imgEl.setAttribute("src", data.sprites.other.dream_world.front_default);
 
   const idEl = document.createElement("p");
   idEl.className = "pokemon-id";
-  idEl.textContent = `#${data.id}`;
+  idEl.textContent = "# " + createId(data.id);
+  function createId(id){
+    if (!id) return null;//!id così scritto indica che non esiste
+    if (id < 10) {return `00${id}`}//se è minore di 10 ritorna 00id
+    else if (id < 100) {return `0${id}`}
+    return id
+  }
 
   const nameEl = document.createElement("h1");
   nameEl.textContent = data.name;
@@ -28,7 +25,7 @@ const createPokemonCard = (data) => {
   typeEl.className = "pokemon-type";
   typeEl.textContent = `Type: ${data.types[0].type.name}`;
 
-  switch (typeEl.textContent) {
+  /*switch (typeEl.textContent) {
     case `Type: grass`:
       cardEl.style.background = "#DEFDE0";
       break;
@@ -85,8 +82,24 @@ const createPokemonCard = (data) => {
       break;
     default:
       cardEl.style.background = "#fff";
-  }
+  }*/
 
   cardEl.append(imgEl, idEl, nameEl, typeEl);
   bodyEl.appendChild(cardEl);
 };
+
+/*fetch("https://pokeapi.co/api/v2/pokemon/?limit=150")
+  .then((response) => response.json())
+  .then((pokemon) => {
+    pokemon.results.forEach((element) =>
+      fetch(element.url).then((response) =>
+        response.json().then((pokemon) => createPokemonCard(pokemon))
+      )
+    );
+  });*/
+
+for (let i = 1; i <= 150; i++) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)//alt+96
+    .then((res) => res.json())
+    .then((res) => createPokemonCard(res));
+}
