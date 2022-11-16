6ebf4a950131d092c49c8a057e5c6b8e4fbde8a6
FrancisCarella <francescodecarocarella@gmail.com>
2022-11-16 17:20:51 +0100

    esercitazione 16-11-22

---
+++ 16-11-22/js/main.js
@@ -0,0 +1,73 @@
+import { GET, POST, DELETE } from "./api.js";
+import { c, q, uuidv4 } from "./utils.js";
+const url = "http://localhost:3000/pokemon";
+
+const form = document.forms.pokemon;
+const element = form.elements;
+const container = q(".pokemon_container");
+
+const ul = q(".pokemon_list");
+
+//FORM SUBMIT
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {
+    // id: uuidv4(),
+    name: element.pkmName.value,
+    type: element.pkmType.value,
+  };
+
+  POST(url, data)
+    .then((response) => response.json())
+    .then((res) => {
+      console.log("Success:", res);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+//FORM LIST
+window.onload = GET(url).then((res) =>
+  res.map(
+    (pkm) => (ul.innerHTML += `<li>#${pkm.id} ${pkm.name}, ${pkm.type}</li>`)
+  )
+);
+
+//CARD CREATION
+const createCard = (res, parent) => {
+  const cardEl = c("div");
+  const imgEl = c("img");
+  const idEl = c("p");
+  const nameEl = c("h1");
+  const typeEl = c("p");
+  const btnEl = c("button");
+
+  cardEl.className = "card";
+  cardEl.classList.add(`bg-${res?.type[0].toLowerCase() + res?.type.slice(1)}`); //Classe per assegnare il background-color
+
+  idEl.className = "pokemon_id";
+  imgEl.className = "pokemon_img";
+  nameEl.className = "pokemon_name";
+  typeEl.className = "pokemon_type";
+  btnEl.className = "pokemon_delete";
+
+  btnEl.addEventListener("click", () => {
+    DELETE(url, res.id).then(() => location.reload());
+  });
+
+  idEl.textContent = "# " + res.id;
+  nameEl.textContent = res.name;
+  typeEl.textContent = `Type: ${res.type}`;
+  imgEl.setAttribute("src", `https://picsum.photos/200?${res?.id}`);
+  imgEl.setAttribute("alt", "image");
+  btnEl.textContent = "delete";
+
+  cardEl.append(imgEl, idEl, nameEl, typeEl, btnEl);
+  parent.append(cardEl);
+};
+
+//CARD LIST
+window.onload = GET(url).then((res) =>
+  res.map((res) => createCard(res, container))
+);
---
+++ 16-11-22/js/utils.js
@@ -0,0 +1,18 @@
+const c = (el) => document.createElement(el);
+const q = (el) => document.querySelector(el);
+
+// API
+/**
+ * Create an unique hash code
+ * @returns string
+ */
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
+export { c, q, uuidv4 };
