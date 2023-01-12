f747c69c536ce1329c50d0d7174427180bb5a627
FrancescoDeCaroCarella <francescodecarocarella@gmail.com>
2023-01-12 16:19:46 +0100

    esercitazione 10-01-23

--- 10-01-23/src/components/number/Number.jsx
+++ 10-01-23/src/components/number/Number.jsx
@@ -4,26 +4,31 @@ import './number.css';
 
 const Number = () => {
 
-    const [numSort, setNumSort] = useState([]);
+    const [numClick, setNumClick] = useState([]);
 
   const handleClick = () => {
     const random = Math.floor(Math.random()*100);
-    setNumSort((current) => [...current, random]);
-    console.log(numSort);
+    setNumClick((current) => [...current, random]);
+    console.log(numClick);
   }
 
   return (
-      <>
-      <button onClick={handleClick} disabled={numSort.length < 5 ? false : true}>Genera un numero</button>
-      {numSort.map((num) => {
+    <>
+      <button
+        onClick={handleClick}
+        disabled={numClick.length < 5 ? false : true}
+      >
+        Genera un numero
+      </button>
+      {numClick.map((num) => {
         return (
           <div className="btn">
             <p>{num}</p>
           </div>
         );
       })}
-      </>
-      )
+    </>
+  );
 }
 
 export default Number;
